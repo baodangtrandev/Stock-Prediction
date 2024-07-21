107a0f1f5560bfb3a301e54f6be801e0c1ba06aa
Tran Dang Bao <trandangbao2004@gmail.com>
2024-07-21 13:51:22 +0700

    add chart with sumary

--- model_prediction.py
+++ model_prediction.py
@@ -3,9 +3,11 @@ import numpy as np
 import yfinance as yf
 from datetime import date
 import streamlit as st
+from plotly import graph_objs as go
 
 
 
+# get data frame
 start = '2014-01-01'
 today = date.today().strftime("%Y-%m-%d")
 
@@ -30,3 +32,27 @@ data_load_state.text("Loading data...done!")
 st.subheader('Data Frame')
 st.write(data.tail())
 
+st.subheader('Summary of the data')
+st.write(data.describe())
+
+st.subheader('Closing Price vs Time Chart')
+
+def plot_chart_price(data):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='Open Price'))
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='Close Price'))
+    fig.layout.update(title_text='Time Series Data', xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig)
+
+plot_chart_price(data)
+
+st.subheader('Volume vs Time Chart')
+
+
+def plot_chart_volume(data):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Volume'], name='Vloume'))
+    fig.layout.update(title_text='Time Series Data', xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig)
+
+plot_chart_volume(data)
