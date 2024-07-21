2172377160bdfb7fde4f552a19396467e0dafd36
Tran Dang Bao <trandangbao2004@gmail.com>
2024-07-21 19:34:47 +0700

    add predict model using LTSM model

--- model_prediction.py
+++ model_prediction.py
@@ -117,17 +117,123 @@ def plot_chart_return_hist(data):
         xaxis=dict(
             tickmode='linear',
             tick0=-10,
-            dtick=1  # Adjust this value for more granular ticks (e.g., 0.01 for 0.01 steps)
+            dtick=1  
         ),
         yaxis=dict(
             tickmode='linear',
             tick0=0,
-            dtick=20  # Adjust this value for more granular ticks (e.g., 0.01 for 0.01 steps)
+            dtick=20  
         ),
         xaxis_rangeslider_visible=True,
-        height=400,  # Adjust the height of the figure
-        bargap=0.1   # Adjust the gap between bars
+        height=400,  
+        bargap=0.1  
     )
     st.plotly_chart(fig)
 
 plot_chart_return_hist(data)
+
+
+
+
+# Train the model and make prediction
+
+from sklearn.preprocessing import MinMaxScaler
+
+
+close_data = data.filter(['Close'])
+close_data = close_data.values
+print(close_data)
+train_data_len = int(np.ceil(len(close_data)*0.9))
+print(len(close_data))
+
+
+
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_data = scaler.fit_transform(close_data)
+print(scaled_data)
+
+
+train_data = scaled_data[0:train_data_len, :]
+
+print("number of training data: ", len(train_data))
+print("train_data: ", train_data)
+
+x_train = []
+y_train = []
+
+for i in range(100, len(train_data)):
+    x_train.append(train_data[i-100:i, 0])
+    y_train.append(train_data[i, 0])
+
+
+x_train = np.array(x_train)
+y_train = np.array(y_train)
+
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1)) # add one more dimension of the input data
+print(x_train.shape)
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0' 
+import tensorflow as tf
+tf.compat.v1.reset_default_graph()
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+
+
+model = Sequential()
+model.add(LSTM(128, return_sequences=True, input_shape= (x_train.shape[1], 1)))
+model.add(LSTM(64, return_sequences=False))
+model.add(Dense(25))
+model.add(Dense(1))
+
+# Compile the model
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+# Train the model
+model.fit(x_train, y_train, batch_size=1, epochs=1)
+
+
+# Test data
+
+test_data = scaled_data[train_data_len - 100:, :]
+
+x_test = []
+y_test = close_data[train_data_len:, :]
+
+for i in range(100, len(test_data)):
+    x_test.append(test_data[i-100:i, 0])
+    
+x_test = np.array(x_test)
+
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+predictions = model.predict(x_test)
+
+predictions = scaler.inverse_transform(predictions)
+
+rmse = np.sqrt(np.mean(predictions - y_test)**2)   
+print(rmse)
+
+
+# Plot the data
+
+train = data[:train_data_len]
+valid = data[train_data_len:]
+valid['Predictions'] = predictions
+
+#visualize the data
+print(valid)
+
+st.subheader('Prediction vs Actual Data')
+
+def plot_chart_prediction(train, valid):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'], y=train['Close'], name='Close Price'))
+    fig.add_trace(go.Scatter(x=valid['Date'], y=valid['Predictions'], name='Predictions'))
+    fig.add_trace(go.Scatter(x=valid['Date'], y=valid['Close'], name='Actual Price'))
+    fig.layout.update(title_text='Time Series Data', xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig)
+
+plot_chart_prediction(train,valid)
+
